24a01de5e467fe2186065c0d34b80d878d22d83b
Robert Tura <robert.tura.fernandez@iaggbs.com>
2025-08-19 17:00:33 +0200

    Starting code before 5pm

--- Landing.py
+++ Landing.py
@@ -1,22 +1,41 @@
-## Demo Streamlit app on EC2
-
-
 import streamlit as st
 import plotly as py
 import plotly.io as pio
 from plotly import graph_objects as go
+from datetime import date
 
 
+with st.sidebar:
+    st.header("Settings")
+    # st.write("Time Window")
+    time_range = st.date_input(
+        "Time Window",
+        [date.today(), date.today()],
+        min_value=None,
+        max_value=None
+    )
+    ac_model = st.multiselect("Select Aircraft Model", 
+                              options = ["A333", "A320", "A21N", "A332", "A20N", "A321", "A319"])
+    # ac_description = 
+    # reg_number = 
+    # ata = 
+    # taskcard = 
+    # amm_code = 
+    # finding_source = 
+    
 
-
-labels =  ["People who like streamlit", "People who don't know about streamlit"]
+labels = ["People who like streamlit", "People who don't know about streamlit"]
 values = [80, 20]
 
 
 # pull is given as a fraction of the pie radius
-fig = go.Figure(data=[go.Pie(labels=labels, values=values,textinfo='label+percent',
-                             insidetextorientation='radial', pull=[0, 0, 0.2, 0] #,hole = 0.2
-                            )])
+fig = go.Figure(data=[go.Pie(
+    labels=labels,
+    values=values,
+    textinfo='label+percent',
+    insidetextorientation='radial',
+    pull=[0, 0]  # Adjusted to match the number of labels
+)])
 
 
 fig.update_layout(
