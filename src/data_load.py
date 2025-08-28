5df038b862e30f8fc80d46acf5b844b4b67d748c
LourdesRojana-IAG <lourdes.rojana.aramburu@iaggbs.com>
2025-08-28 16:01:50 +0200

    add data

--- src/data_load.py
+++ src/data_load.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def get_local_csv_data():
     #Data loading
-    df = pd.read_csv("../data/aerlingus_llm_enrichment_columns_23-08-2025.csv")
+    df = pd.read_csv("src/data/aerlingus_llm_enrichment_columns_23-08-2025.csv")
     df['Date'] = pd.to_datetime(df['issue_date'])
     return df
 
