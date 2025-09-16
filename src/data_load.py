c2e9b781495fc40ecc44dd742d7c8c011a043089
Cristian Escudero <cristian.escudero@aktios.com>
2025-09-16 14:59:46 +0200

    updated new defects csv

--- src/data_load.py
+++ src/data_load.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def get_local_csv_data():
     #Data loading
-    df = pd.read_csv("src/data/llm_enhancement_aerlingus_full_25-08-2025.csv", sep = ';')
+    df = pd.read_csv("src/data/llm_enhancement_aerlingus_defects_droppedna.csv", sep = ';')
     df['Date'] = pd.to_datetime(df['issue_date'])
     return df
 
